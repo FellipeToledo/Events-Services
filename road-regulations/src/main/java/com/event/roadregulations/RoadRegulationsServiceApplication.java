b8c74b43ca1d46910ffcfe3756d3b66a4059b6bc
FellipeToledo <fellipetoledo.dev@gmail.com>
2023-03-17 14:47:42 -0300

    road regulations service added

---
+++ road-regulations/src/main/java/com/event/roadregulations/RoadRegulationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.event.roadregulations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadRegulationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadRegulationsServiceApplication.class, args);
+	}
+
+}
---
+++ road-regulations/src/test/java/com/event/roadregulations/RoadRegulationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.roadregulations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadRegulationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
